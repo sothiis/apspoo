5aae31f89d7727b9134dc87a5b438024c485f4a1
sothiis <21424597@anhembimorumbi.edu.br>
2021-05-17 14:45:04 -0300

    second

--- Conta.java
+++ Conta.java
@@ -1,3 +1,11 @@
-public class Conta {
-    // A classe ​Conta​ é uma classe abstrata.
+public abstract class Conta {
+  private int numero;
+  private double saldo;
+
+  public Conta(){}
+  public Conta(int numero, double saldo){
+      this.numero = numero;
+      this.saldo = saldo;
+  }
+
 }
