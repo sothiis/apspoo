076cca6ecf13a4191a46b735c60edbc1b0aa8cb3
sothiis <21424597@anhembimorumbi.edu.br>
2021-05-17 15:15:02 -0300

    second

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,3 +1,7 @@
 public class ContaCorrente extends Conta {
    //A ​ContaCorrente​ permite fazer saques somente se houver saldo suficiente. 
+
+   
+
+
 }
--- GerenciaConta.java
+++ GerenciaConta.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+
 public class GerenciaConta {
-    
+    private ArrayList<> listaDeContas;
+
+    public GerenciaConta(){
+        listaDeContas = new ArrayList<>();
+    }
+
+    public void novaContaCorrente(){
+
+    }
+
+    public void novaContaEspecial(){
+
+    }
+
+    public void novaContaPoupanca(){
+
+    }
+
+    public String listarContas(){
+        String saida ="";
+    }
+
 }
